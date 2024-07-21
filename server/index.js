0a54523801f4a30ca65b8df3d697e6158deb2f8b
Swathika-P <swathika.p2021csec@sece.ac.in>
2024-07-22 00:14:07 +0530

    modify cors origin with APPLICATION_URL

--- server/index.js
+++ server/index.js
@@ -22,6 +22,12 @@ import { users, posts } from "./data/index.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
+
+const corsOptions = {
+  origin: process.env.APPLICATION_URL,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
+};
+
 const app = express();
 app.use(express.json());
 app.use(helmet());
@@ -30,6 +36,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use(cors(corsOptions));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
